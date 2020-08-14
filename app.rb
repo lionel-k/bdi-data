c47150321108190e076dabad85b5da8d9495e383
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-08-14 19:43:34 +0200

    Move to ruby

--- app.rb
+++ app.rb
@@ -1,3 +1,18 @@
 #!/usr/bin/ruby
 
-puts
+# Reads the variables sent via POST
+session_id   = $_POST["sessionId"];
+service_code = $_POST["serviceCode"];
+text = $_POST["text"];
+
+# This is the first menu screen
+
+if text == ''
+  response  = 'CON Hi welcome, I can help you with Event Reservation \n'
+  response .= '1. Enter 1 to continue'
+  # $response = "Hello".$_POST;
+end
+
+header('Content-type: text/plain')
+puts response
+
