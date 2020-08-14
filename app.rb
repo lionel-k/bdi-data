0c3c6306c5ea1714f11479ab5e99a513b32e532c
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-08-14 19:56:52 +0200

    Move to sinatra

--- app.rb
+++ app.rb
@@ -1,18 +1,22 @@
-#!/usr/bin/ruby
-
 # Reads the variables sent via POST
-session_id   = $_POST["sessionId"];
-service_code = $_POST["serviceCode"];
-text = $_POST["text"];
+# session_id   = $_POST["sessionId"];
+# service_code = $_POST["serviceCode"];
+# text = $_POST["text"];
 
-# This is the first menu screen
+# # This is the first menu screen
 
-if text == ''
-  response  = 'CON Hi welcome, I can help you with Event Reservation \n'
-  response .= '1. Enter 1 to continue'
-  # $response = "Hello".$_POST;
+# if text == ''
+#   response  = 'CON Hi welcome, I can help you with Event Reservation \n'
+#   response .= '1. Enter 1 to continue'
+#   # $response = "Hello".$_POST;
+# end
+
+# header('Content-type: text/plain')
+# return response
+
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+  'Hello from Sinatra on Heroku!'
 end
-
-header('Content-type: text/plain')
-return response
-
