2b8cb6e8bb6b4e0cd5487d59e2bdd6e47df8b270
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-08-15 04:23:09 +0200

    Add first version of ussd

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ require 'pry'
 set :database_file, 'config/database.yml'
 
 get '/' do
-  'Welcome the open data portal on Burundi'
+  'Welcome to Burundi Data'
 end
 
 get '/exchangerates' do
@@ -42,13 +42,39 @@ get '/exchangerates/:date' do
   return_currencies(exchange_rate)
 end
 
-post '/' do
-  session_id   = params['sessionId']
-  service_code = params['serviceCode']
-  phone_number = params['phoneNumber']
-  network_code = params['networkCode']
-  text = params['text']
-  response = "CON Hi welcome, I can help you with Event Reservation \n"
+post '/ussd' do
+  session_id = params[:sessionId]
+  service_code = params[:serviceCode]
+  phone_number = params[:phoneNumber]
+  text = params[:text]
+
+  puts params
+
+  puts "text: #{text} by #{phone_number}"
+
+  if text == ''
+    response  = "CON Karibu kuri Burundi Data? \n"
+    response  = "Twogushaka gute ? \n"
+    response += "1. Amafaranga avunjwa gute ? \n"
+    response += "2. Haraza kurwa invura ?"
+
+  elsif text == '1'
+    exchange_rate = ExchangeRate.find_by(date: Date.today)
+    rates = JSON.parse(return_currencies(exchange_rate))
+    rates = rates.map { |k, v| "#{k}: #{v}" }.join("\n")
+    response = "END #{rates}"
+
+  elsif text == '2'
+    will_rain  = 'Ego'
+    response = "CON Uri muri province iyahe ?"
+
+  elsif text == '2*1'
+    will_rain  = 'Ego'
+    response = "END #{will_rain}"
+  end
+
+  body response
+  status 200
 end
 
 def return_currencies(exchange_rate)
