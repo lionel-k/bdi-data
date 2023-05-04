bfe77c215e5e6108fa5c3b2eaeb7c7388797a765
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2023-05-04 20:25:01 +0200

    Update app.rb and lib/tasks/update_rates.rake files to support Exchange Rate fetching and updating

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'date'
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/json'
-require "sinatra/reloader" if development?
+require 'sinatra/reloader' if development?
 require 'pry'
 
 set :database_file, 'config/database.yml'
@@ -25,18 +25,15 @@ end
 
 get '/exchangerates/:date' do
   begin
-     date = Date.strptime(params[:date],"%Y-%m-%d")
+    date = Date.strptime(params[:date], '%Y-%m-%d')
   rescue ArgumentError
-     response = { message: 'invalid date - format: year-month-date' }
-     return json response
+    response = { message: 'invalid date - format: year-month-date' }
+    return json response
   end
 
   exchange_rate = ExchangeRate.find_by(date: date)
   if exchange_rate.nil?
-    response = {
-      date: date,
-      message: 'Exchange Rate at this date not available'
-    }
+    response = { date: date, message: 'Exchange Rate at this date not available' }
     return json response
   end
 
@@ -54,24 +51,21 @@ post '/ussd' do
   puts "text: #{text} by #{phone_number}"
 
   if text == ''
-    response  = "CON Karibu kuri Burundi Data? \n"
+    response = "CON Karibu kuri Burundi Data? \n"
     response += "Twobafasha gute ? \n"
     response += "1. Amafaranga avunjwa gute ? \n"
     response += '2. Haraza kurwa imvura ?'
-
   elsif text == '1'
     exchange_rate = ExchangeRate.find_by(date: Date.today)
     rates = JSON.parse(return_currencies(exchange_rate))
     rates = rates.map { |k, v| "#{k}: #{v}" }.join("\n")
     response = "END #{rates}"
-
   elsif text == '2'
-    will_rain  = 'Ego'
-    response = "CON Uri muri ntara iyahe ?"
-
+    will_rain = 'Ego'
+    response = 'CON Uri muri ntara iyahe ?'
   elsif text.match(/2\*\w+/)
     region = text.match(/2\*(\w+)/)[1]
-    will_rain  = ['Ego', 'Oya'].sample
+    will_rain = %w[Ego Oya].sample
     response = "END Mu ntara ya #{region} : #{will_rain}"
   end
 
--- lib/tasks/update_rates.rake
+++ lib/tasks/update_rates.rake
@@ -4,20 +4,21 @@ require 'nokogiri'
 desc 'Add latest rate from BRB website'
 task :update_rates do
   html_content = open('https://brb.bi/').read
-  doc = Nokogiri::HTML(html_content)
+  doc = Nokogiri.HTML(html_content)
 
   exchange_rate = ExchangeRate.find_or_create_by(date: Date.today)
   exchange_rate.currencies.destroy_all
 
   table = doc.search('.views-table')
-  rows = table.xpath("//table/tbody/tr")
-  rates = rows.map do |row|
-    columns = row.xpath('td')
-    name = columns[0].text.strip
-    buyer = columns[1].text.strip.to_f
-    seller = columns[2].text.strip.to_f
-    rate = ((buyer + seller) / 2).round(2)
-    { name: name, buyer: buyer, seller: seller, rate: rate }
-  end
+  rows = table.xpath('//table/tbody/tr')
+  rates =
+    rows.map do |row|
+      columns = row.xpath('td')
+      name = columns[0].text.strip
+      buyer = columns[1].text.strip.to_f
+      seller = columns[2].text.strip.to_f
+      rate = ((buyer + seller) / 2).round(2)
+      { name: name, buyer: buyer, seller: seller, rate: rate }
+    end
   exchange_rate.currencies.create!(rates)
 end
